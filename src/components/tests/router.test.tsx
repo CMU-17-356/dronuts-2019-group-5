87c0fbdaa2f01f81b8afad59576d023d6b559353
Bobbie Chen <bobbiechen75@gmail.com>
2019-02-12 19:31:52 -0500

    Move component tests to components/tests

--- src/components/tests/router.test.tsx
+++ src/components/tests/router.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import HashRouter from 'react-router';
-import AppRouter from '../router';
+import AppRouter from '../../router';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
