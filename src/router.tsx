af6e68ba33856da6685614ab7ed072db90803a50
shumry <ruiyuans@andrew.cmu.edu>
2019-02-13 19:40:51 -0500

    Reloading commit

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Route, HashRouter, Switch } from 'react-router-dom';
-import { Hello, Menu, Header } from './components';
+import { Hello, Menu, Header, Inventory } from './components';
 
 export const AppRouter: React.StatelessComponent<{}> = () => {
   return (
@@ -10,7 +10,9 @@ export const AppRouter: React.StatelessComponent<{}> = () => {
         <Switch>
           <Route exact path="/" render={(props) => <Hello {...props} name="page 1" /> } />
           <Route path="/hello1" render={(props) => <Hello {...props} name="page 1" /> } />
-          <Route path="/inventory" render={(props) => <Inventory {...props} name="inventory" /> } />
+          <Route path="/inventory" render={(props) => <Inventory {...props} items={[
+              {id: "1", name: "Original Glazed", ingredients: "wheat_flour, sugar", priceInCents: 100, inStock: true},
+              {id: "2", name: "Chocolate Glazed", ingredients: "white_flour, chocolate, butter", priceInCents: 150, inStock: true,}]} /> } />
           <Route path="/hello3" render={(props) => <Hello {...props} name="page 3" /> } />
           <Route path="/menu" render={(props) =>
             <Menu {...props} items={[
